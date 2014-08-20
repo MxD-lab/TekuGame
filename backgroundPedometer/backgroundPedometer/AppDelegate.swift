9eba06985f589d94ac2995644e045dac32086756
Stefan Alexander <sualexander@wpi.edu>
2014-08-20 15:54:45 +0900

    initial commit of background pedometer

---
+++ backgroundPedometer/backgroundPedometer/AppDelegate.swift
@@ -0,0 +1,114 @@
+//
+//  AppDelegate.swift
+//  backgroundPedometer
+//
+//  Created by ステファンアレクサンダー on 2014/08/20.
+//  Copyright (c) 2014年 ステファンアレクサンダー. All rights reserved.
+//
+
+// http://stackoverflow.com/questions/16647628/ios-coremotion-accelerator-updates-in-background-sleepmode
+// http://stackoverflow.com/questions/15092016/how-to-run-nstimer-in-background-and-sleep-in-ios
+
+import UIKit
+import CoreMotion
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+                            
+    var window: UIWindow?
+    var prevSteps:Int! = 0
+    var stepCount:Int! = 0
+
+    func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication!) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(application: UIApplication!) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        var lowQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0);
+
+        getHistoricalSteps()
+        updateSteps()
+        
+        UIApplication.sharedApplication().beginBackgroundTaskWithExpirationHandler(nil)
+        var loop = NSTimer.scheduledTimerWithTimeInterval(10, target: self, selector: Selector("notifySteps"), userInfo: nil, repeats: true)
+        NSRunLoop.currentRunLoop().addTimer(loop, forMode: NSRunLoopCommonModes)
+    }
+
+    func applicationWillEnterForeground(application: UIApplication!) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(application: UIApplication!) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication!) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    // Gets the number of steps taken from the start of the day to the current time.
+    func getHistoricalSteps() {
+        if(CMStepCounter.isStepCountingAvailable()){
+            var stepCounter = CMStepCounter()
+            var mainQueue:NSOperationQueue! = NSOperationQueue()
+            var todate:NSDate! = NSDate()
+            
+            var lowQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0);
+            
+            dispatch_async(lowQueue, { () -> Void in
+                stepCounter.queryStepCountStartingFrom(self.startDateOfToday(), to: todate, toQueue: mainQueue, withHandler: {numberOfSteps, error in
+                    self.prevSteps = numberOfSteps
+                })
+            })
+        }
+    }
+    
+    // Starts counting the number of steps.
+    func updateSteps() {
+        if(CMStepCounter.isStepCountingAvailable()){
+            var stepCounter = CMStepCounter()
+            var mainQueue:NSOperationQueue! = NSOperationQueue()
+            var todate:NSDate! = NSDate()
+            
+            var lowQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0);
+            
+            dispatch_async(lowQueue, { () -> Void in
+                stepCounter.startStepCountingUpdatesToQueue(mainQueue, updateOn: 1, withHandler: {numberOfSteps, timestamp, error in
+                    self.stepCount = numberOfSteps + self.prevSteps
+                })
+            })
+        }
+    }
+    
+    func notifySteps() {
+
+        var lowQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0);
+        
+        dispatch_async(lowQueue, { () -> Void in
+            if (self.stepCount % 100 == 0) {
+                var notification = UILocalNotification()
+                notification.fireDate = NSDate()
+                notification.alertBody = "\(self.stepCount) steps."
+                println("\(self.stepCount) steps.")
+                UIApplication.sharedApplication().scheduleLocalNotification(notification)
+            }
+        })
+    }
+    
+    // Returns an NSDate object of the beginning of the day.
+    func startDateOfToday() -> NSDate! {
+        var calender = NSCalendar.currentCalendar()
+        var components = calender.components(.CalendarUnitYear | .CalendarUnitMonth | .CalendarUnitDay, fromDate: NSDate())
+        return calender.dateFromComponents(components)
+    }
+
+}
+
